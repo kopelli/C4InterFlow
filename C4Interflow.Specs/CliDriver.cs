6e7fb601c0203fca40d2f5af672e92b0e906a401
Steven Evans <kopelli+github@gmail.com>
2024-09-30 10:31:59 -0500

    WIP - test suite.
    Works individually, but not in bulk

---
+++ C4Interflow.Specs/CliDriver.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Concurrent;
+using System.CommandLine.Builder;
+using System.CommandLine.Parsing;
+using C4InterFlow.Cli.Commands;
+using C4InterFlow.Cli.Extensions;
+using C4InterFlow.Cli.Root;
+
+namespace C4Interflow.Specs
+{
+    public class CliDriverManager
+    {
+        private readonly ConcurrentDictionary<string, CliDriver> drivers = [];
+
+        public CliDriver ForContext(string context)
+        {
+            Console.WriteLine($"For context '{context}'");
+            drivers.AddOrUpdate(context, new CliDriver(), (c, d) => d);
+            return drivers[context];
+        }
+    }
+    public class CliDriver
+    {
+        public CliDriver()
+        {
+            cliArgs = [];
+        }
+
+        private readonly List<string> cliArgs;
+
+        public string SampleRootName { get; set; }
+        public string WorkingDirectory { get; set; }
+
+        public async Task<int> BuildAndInvoke()
+        {
+            var args = cliArgs.ToArray();
+            Console.WriteLine(string.Join(" ", args));
+            var rootCommandBuilder = RootCommandBuilder
+                .CreateDefaultBuilder(args)
+                .Configure(context =>
+                {
+                    context.Add<DrawDiagramsCommand>();
+                    context.Add<QueryUseFlowsCommand>();
+                    context.Add<QueryByInputCommand>();
+                    context.Add<ExecuteAaCStrategyCommand>();
+                    context.Add<GenerateDocumentationCommand>();
+                    context.Add<PublishSiteCommand>();
+                });
+
+            return await new CommandLineBuilder(rootCommandBuilder.Build())
+                .UseDefaults().UseLogging().Build().InvokeAsync(args);
+        }
+
+        public void WithAaCInputPath(string path)
+        {
+            cliArgs.Add("--aac-input-paths");
+            cliArgs.Add($"{WorkingDirectory}\\{SampleRootName}\\{path}");
+        }
+
+        public void WithAaCReaderStrategy(Type readerType)
+        {
+            cliArgs.Add("--aac-reader-strategy");
+            cliArgs.Add(readerType.AssemblyQualifiedName);
+        }
+
+        public void WithInterfaces(string interfaceQuery)
+        {
+            cliArgs.Add("--interfaces");
+            cliArgs.AddRange(interfaceQuery.Split(' '));
+        }
+
+        public void WithBusinessProcesses(string businessQuery)
+        {
+            cliArgs.Add("--business-processes");
+            cliArgs.AddRange(businessQuery.Split(' '));
+        }
+
+        public void WithLevelOfDetails(string levelOfDetails)
+        {
+            cliArgs.Add("--levels-of-details");
+            cliArgs.AddRange(levelOfDetails.Split(' '));
+        }
+
+        public void OutputTo(string outputPath)
+        {
+            cliArgs.Add("--output-dir");
+            cliArgs.Add(outputPath);
+        }
+
+        public void ForCommand(string rootCommand)
+        {
+            cliArgs.Clear();
+            cliArgs.Add(rootCommand);
+        }
+    }
+}
---
+++ C4Interflow.Specs/EnvironmentStepDefinitions.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Reflection;
+using C4InterFlow.Automation.Readers;
+using TechTalk.SpecFlow;
+
+[assembly: Microsoft.VisualStudio.TestTools.UnitTesting.Parallelize(Scope = ExecutionScope.MethodLevel)]
+
+namespace C4Interflow.Specs
+{
+    [Binding]
+    public sealed class EnvironmentStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private CliDriverManager _drivers;
+        private string _tempPath;
+        private int _cliExitCode;
+
+        public EnvironmentStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+            _drivers = new();
+        }
+
+        [BeforeScenario]
+        public void UpdateScenarioRun(ITestRunnerManager testRunnerManager)
+        {
+            _tempPath = Path.Combine(Path.GetTempPath(), Assembly.GetExecutingAssembly().GetName().Name, Guid.NewGuid().ToString("N"));
+            Environment.CurrentDirectory = Path.GetFullPath("../../../../../Samples", Assembly.GetExecutingAssembly().Location);
+            Console.WriteLine("Before scenario...");
+        }
+
+        [AfterScenario]
+        public void AfterScenario()
+        {
+            Assert.AreEqual(0, _cliExitCode, "The command exited with a non-zero value.");
+        }
+
+        [Given(@"the '(.+)' command")]
+        public void ForCommand(string command)
+        {
+            _drivers.ForContext(_scenarioContext.Description()).ForCommand(command);
+        }
+
+        [Given(@"the '(.+)' example")]
+        public void GivenTheExample(string example)
+        {
+            _drivers.ForContext(_scenarioContext.Description()).WorkingDirectory =
+                Path.GetFullPath("../../../../../Samples", Assembly.GetExecutingAssembly().Location);
+            _drivers.ForContext(_scenarioContext.Description()).SampleRootName = example;
+        }
+
+        [Given(@"the path '(.+)'")]
+        public void GivenThePath(string path)
+        {
+            _drivers.ForContext(_scenarioContext.Description()).WithAaCInputPath(path);
+        }
+
+        [Given(@"the reader strategy is '(.+)'")]
+        public void GivenTheReaderStrategy(string strategy)
+        {
+            switch (strategy)
+            {
+                case "Yaml":
+                    _drivers.ForContext(_scenarioContext.Description()).WithAaCReaderStrategy(typeof(YamlAaCReaderStrategy));
+                    break;
+                case "Json":
+                    _drivers.ForContext(_scenarioContext.Description()).WithAaCReaderStrategy(typeof(JsonAaCReaderStrategy));
+                    break;
+                default:
+                    Assert.Fail($"Unknown strategy: '{strategy}'");
+                    break;
+            }
+        }
+
+        [Given(@"the interfaces are '(.*)'")]
+        public void GivenTheInterfacesAre(string interfaceQuery)
+        {
+            _drivers.ForContext(_scenarioContext.Description()).WithInterfaces(interfaceQuery);
+        }
+
+        [Given(@"the business processes are '(.*)'")]
+        public void GivenTheBusinessProcessesAre(string businessQuery)
+        {
+            _drivers.ForContext(_scenarioContext.Description()).WithBusinessProcesses(businessQuery);
+        }
+
+
+        [Given(@"the level of details is '(.*)'")]
+        public void GivenTheLevelOfDetailsIs(string levelOfDetails)
+        {
+            _drivers.ForContext(_scenarioContext.Description()).WithLevelOfDetails(levelOfDetails);
+        }
+
+        [Given(@"send the output to '(.*)'")]
+        public void GivenSendTheOutputTo(string outputPath)
+        {
+            if (!Path.IsPathRooted(outputPath))
+            {
+                outputPath = Path.Combine(_tempPath, outputPath);
+            }
+
+            if (!Directory.Exists(outputPath))
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+            _drivers.ForContext(_scenarioContext.Description()).OutputTo(outputPath);
+        }
+
+        [When(@"invoking the commandline for those arguments")]
+        public void WhenInvokingTheCommandlineForThoseArguments()
+        {
+            _cliExitCode = _drivers.ForContext(_scenarioContext.Description()).BuildAndInvoke().Result;
+        }
+
+        [Then(@"all files under '(.*)' should match example path '(.*)'")]
+        public void ThenAllFilesUnderShouldMatchExamplePath(string actualPath, string expectedPath)
+        {
+            if (!Path.IsPathRooted(actualPath))
+            {
+                actualPath = Path.Combine(_tempPath, actualPath);
+            }
+
+            expectedPath = Path.Combine(Directory.GetCurrentDirectory(), expectedPath);
+
+            var getFileList = (string path) => Directory.GetFiles(path, "*", SearchOption.AllDirectories)
+                .Select(p => new { FullPath = p, RelativePath = Path.GetRelativePath(path, p) }).ToArray();
+
+            Console.WriteLine(expectedPath);
+            Console.WriteLine(actualPath);
+            var expectedFiles = getFileList(expectedPath);
+            var actualFiles = getFileList(actualPath);
+
+            Assert.AreNotEqual(0, actualFiles.Length, $"No files were generated under '{actualPath}'.");
+
+            // We cannot compare the raw file count because what's in the Sample directory will always have more files.
+            // Instead, we have to compare based on the Relative paths.
+
+            var actualRelativePaths = actualFiles.ToDictionary(x => x.RelativePath, x => x);
+            var expectedRelativePaths = expectedFiles.ToDictionary(x => x.RelativePath, x => x);
+
+            var extraFiles = actualRelativePaths.Keys.Except(expectedRelativePaths.Keys).ToArray();
+            Assert.AreEqual(0, extraFiles.Length, "There are extra files in the output.");
+
+            foreach (var kvp in actualRelativePaths)
+            {
+                var actualFullPath = kvp.Value.FullPath;
+                var expectedFullPath = expectedRelativePaths[kvp.Key].FullPath;
+
+                var actualContents = File.ReadAllText(actualFullPath);
+                var expectedContents = File.ReadAllText(expectedFullPath);
+                Assert.AreEqual(expectedContents, actualContents, $"The contents of '{actualFullPath}' did not match the contents of '{expectedFullPath}'");
+            }
+        }
+    }
+
+    internal static class ScenarioContextExtensions
+    {
+        public static string Description(this ScenarioContext scenarioContext)
+        {
+            return scenarioContext.ScenarioInfo?.Arguments["Description"]?.ToString() ?? string.Empty;
+        }
+    }
+}
+
